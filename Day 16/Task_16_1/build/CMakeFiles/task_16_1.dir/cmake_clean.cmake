6c9e84d7d2ed54b429d0574737cfe699a4ae4556
IOFeed <m.vysotskyi@gmail.com>
2014-02-12 23:11:04 +0200

    12/02/2014

---
+++ Day 16/Task_16_1/build/CMakeFiles/task_16_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/task_16_1.dir/main.o"
+  "task_16_1.pdb"
+  "task_16_1"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/task_16_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
